4d9aba58d1de5c2fd6d006078ac2e0dff3f2947d
Isis de Oliveira <isisd.oliveira@hotmail.com>
2022-05-20 22:23:15 -0300

    My first project

---
+++ src/main/Principal.java
@@ -0,0 +1,15 @@
+package main;
+
+import model.Pessoa;
+
+public class Principal {
+    public static void main (String args[]){
+        Pessoa p = new Pessoa();
+        p.setNome("Isis");
+        p.setIdade(33);
+
+        //System.out.print(p.getNome(),p.getIdade());
+        //Imprimir nome e idade
+        //Instanciar uma segunda pessoa e imprimi-la
+    }
+}
